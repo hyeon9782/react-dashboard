0fca25fcba640949c7233161114465e3c48e9bbb
jeongho <hyeon9782@gmail.com>
2024-04-21 15:27:53 +0900

    feat: highchart 의존성 추가 및 chart 컴포넌트 구현

---
+++ src/components/chart/BasicChart.tsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+    text: 'My chart'
+  },
+  series: [{
+    data: [1, 2, 3]
+  }]
+}
+
+const BasicChart = (props: HighchartsReact.Props) => {
+    const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      ref={chartComponentRef}
+      {...props}
+    />
+  );
+}
+
+export default BasicChart
