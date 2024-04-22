7a72ad03829393a2c6ee98d3754eef53f21fbb60
jeongho <hyeon9782@gmail.com>
2024-04-22 23:43:38 +0900

    chore & feat: react-query 의존성 추가 및 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from '@/router/AppRouter.tsx';
 import { worker } from '../src/mocks/browser';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
 
+const quetyClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={quetyClient}>
+      <AppRouter />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
