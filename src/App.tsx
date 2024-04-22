f5b7222071d615cbd2c9b7b04ff4f4db1dba52bf
jeongho <hyeon9782@gmail.com>
2024-04-22 21:56:22 +0900

    chore & feat: tailwind css 의존성 추가 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import Header from './components/layout/Header';
+import SideBar from './components/layout/SideBar';
 
 function App() {
-  return <Outlet />;
+  return (
+    <div className="flex">
+      <SideBar />
+      <div className="">
+        <Header />
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default App;
