24bd83be433b209d7058685fb6e486d1462631a0
jeongho <hyeon9782@gmail.com>
2024-04-25 22:46:15 +0900

    feat: searched glycoprotein api 호출 함수 작성

--- src/apis/searched/index.ts
+++ src/apis/searched/index.ts
@@ -32,5 +32,42 @@ const getProteinIdentificationCharts = (type: string, fileIds: string) => {
     return instance.get(`/v2/api/protein/identification-charts/${type}/${fileIds}`)
 }
 
+/* =================================================================================================== */
 
-export {getProteinSummaryTables, getProteinSummaryCharts,getProteinQuantificationTables, getProteinQuantificationTablesAll,getProteinQuantificationCharts, getProteinIdentificationTables, getProteinIdentificationTablesAll,  getProteinIdentificationCharts  }
+const getGlycoproteinSummaryTables = (analysisNo: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/summary-tables/${analysisNo}`)
+}
+
+const getGlycoproteinSummaryCharts = (fileId: string, type: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/summary-charts/${type}/${fileId}`)
+}
+
+const getGlycoproteinQuantificationTables = (fileIds: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/quantification-tables/${fileIds}`)
+}
+
+const getGlycoproteinQuantificationTablesAll = (fileIds: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/quantification-tables/${fileIds}/all`)
+}
+
+const getGlycoproteinQuantificationCharts = (type: string, fileIds: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/quantification-charts/${type}/${fileIds}`)
+}
+
+const getGlycoproteinIdentificationTables = (fileIds: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/identification-tables/${fileIds}`)
+}
+
+const getGlycoproteinIdentificationTablesAll = (fileIds: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/identification-tables/${fileIds}/all`)
+}
+
+const getGlycoproteinIdentificationCharts = (type: string, fileIds: string) => {
+    return instance.get(`/v2/api/glycoprotein/searched/identification-charts/${type}/${fileIds}`)
+}
+
+
+export {getProteinSummaryTables, getProteinSummaryCharts,getProteinQuantificationTables, getProteinQuantificationTablesAll,getProteinQuantificationCharts, getProteinIdentificationTables, getProteinIdentificationTablesAll,  getProteinIdentificationCharts,
+
+    getGlycoproteinSummaryTables, getGlycoproteinSummaryCharts,getGlycoproteinQuantificationTables, getGlycoproteinQuantificationTablesAll,getGlycoproteinQuantificationCharts, getGlycoproteinIdentificationTables, getGlycoproteinIdentificationTablesAll,  getGlycoproteinIdentificationCharts
+  }
