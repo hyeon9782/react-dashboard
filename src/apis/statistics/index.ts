a053510c2979bcc611b17d8e80e571fd29b65aa6
jeongho <hyeon9782@gmail.com>
2024-04-25 22:40:43 +0900

    feat: statistics glycoprotein api 호출 함수 작성

--- src/apis/statistics/index.ts
+++ src/apis/statistics/index.ts
@@ -32,4 +32,42 @@ const getProteinDifferentialCharts = (analysisNo: string, type: string, dataType
     return instance.get(`/v2/api/protein/statistics/differential-charts/${type}/${dataType}/${analysisNo}`)
 }
 
-export {getProteinProcessingTables, getProteinProcessingVennCharts, getProteinProcessingQqplotCharts, getProteinNormalizationTables, getProteinNormalizationChartsType, getProteinNormalizationChartsGroup, getProteinDifferentialTables, getProteinDifferentialCharts }
+/* ======================================================================================================== */
+
+const getGlycoproteinProcessingTables = (analysisNo: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/processing-tables/${analysisNo}`)
+}
+
+const getGlycoproteinProcessingVennCharts = (type: string , analysisNo: string, name: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/processing-charts/venn/${type}/${name}/${analysisNo}`)
+}
+
+const getGlycoproteinProcessingQqplotCharts = (groupName: string , analysisNo: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/processing-charts/qqplot/${groupName}/${analysisNo}`)
+}
+
+const getGlycoproteinNormalizationTables = (analysisNo: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/normalization-tables/${analysisNo}`)
+}
+
+const getGlycoproteinNormalizationChartsType = (analysisNo: string, dataType: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/normalization-charts-type/${dataType}/${analysisNo}`)
+}
+
+const getGlycoproteinNormalizationChartsGroup = (analysisNo: string, type: string, name: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/normalization-charts-group/${type}/${name}/${analysisNo}`)
+}
+
+const getGlycoproteinDifferentialTables = (analysisNo: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/differential-tables/${analysisNo}`)
+}
+
+const getGlycoproteinDifferentialCharts = (analysisNo: string, type: string, dataType: string) => {
+    return instance.get(`/v2/api/glycoprotein/statistics/differential-charts/${type}/${dataType}/${analysisNo}`)
+}
+
+export {getProteinProcessingTables, getProteinProcessingVennCharts, getProteinProcessingQqplotCharts, getProteinNormalizationTables, getProteinNormalizationChartsType, getProteinNormalizationChartsGroup, getProteinDifferentialTables, getProteinDifferentialCharts, 
+
+
+    getGlycoproteinProcessingTables, getGlycoproteinProcessingVennCharts, getGlycoproteinProcessingQqplotCharts, getGlycoproteinNormalizationTables, getGlycoproteinNormalizationChartsType, getGlycoproteinNormalizationChartsGroup, getGlycoproteinDifferentialTables, getGlycoproteinDifferentialCharts
+ }
